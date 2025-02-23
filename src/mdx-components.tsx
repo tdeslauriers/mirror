1f42c5e04e6320b081142a0a50403e7a73b0a66d
tdeslauriers <thomas.deslauriers@gmail.com>
2025-02-23 17:52:38 -0600

    adding markdown vs html

---
+++ src/mdx-components.tsx
@@ -0,0 +1,23 @@
+import type { MDXComponents } from "mdx/types";
+import Link from "next/link";
+
+export function useMDXComponents(components: MDXComponents): MDXComponents {
+  return {
+    strong: ({ children }) => <span className="highlight">{children}</span>,
+    a: ({ href, children, ...props }) => {
+      // check for href
+      if (!href) return <a {...props}>{children}</a>;
+      return (
+        <Link
+          className="locallink"
+          href={href}
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          {children}
+        </Link>
+      );
+    },
+    ...components,
+  };
+}
